490f1c1b201dfb8863738dd12c71a98a8f53d8ca
meeklife <forsonbaah@gmail.com>
2023-05-01 19:50:16 +0000

    added a small fix to my search function

--- assets/js/main.js
+++ assets/js/main.js
@@ -303,14 +303,22 @@ function searchTable() {
 
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[2];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
+    let foundMatch = false;
+    const tds = tr[i].getElementsByTagName("td");
+    for (j = 0; j < tds.length; j++) {
+      td = tds[j];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          foundMatch = true;
+          break;
+        }
       }
+    }
+    if (foundMatch) {
+      tr[i].style.display = "";
+    } else {
+      tr[i].style.display = "none";
     }
   }
 }
